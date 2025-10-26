3a94820db2a273c5c80c521e18098d691803f653
Ali Moradi <ali.mrd318@gmail.com>
2025-10-26 12:38:26 +0330

    Refactor spacing in SimpleToken contract and tests for consistency
    - Adjusted spacing in the INITIAL_SUPPLY constant definition for better readability.
    - Updated test cases to maintain consistent spacing in calculations involving token amounts.
    - Ensured no functional changes were made, only formatting improvements.

--- test/SimpleToken.t.sol
+++ test/SimpleToken.t.sol
@@ -14,13 +14,13 @@ contract SimpleTokenTest is Test {
         owner = address(this);
         user1 = address(0x1);
         user2 = address(0x2);
-        
+
         token = new SimpleToken();
     }
 
     function testInitialSupply() public {
-        assertEq(token.totalSupply(), 1000000 * 10**18);
-        assertEq(token.balanceOf(owner), 1000000 * 10**18);
+        assertEq(token.totalSupply(), 1000000 * 10 ** 18);
+        assertEq(token.balanceOf(owner), 1000000 * 10 ** 18);
     }
 
     function testTokenDetails() public {
@@ -30,38 +30,38 @@ contract SimpleTokenTest is Test {
     }
 
     function testTransfer() public {
-        uint256 transferAmount = 1000 * 10**18;
-        
+        uint256 transferAmount = 1000 * 10 ** 18;
+
         assertTrue(token.transfer(user1, transferAmount));
         assertEq(token.balanceOf(user1), transferAmount);
-        assertEq(token.balanceOf(owner), 1000000 * 10**18 - transferAmount);
+        assertEq(token.balanceOf(owner), 1000000 * 10 ** 18 - transferAmount);
     }
 
     function testMint() public {
-        uint256 mintAmount = 5000 * 10**18;
+        uint256 mintAmount = 5000 * 10 ** 18;
         uint256 initialSupply = token.totalSupply();
-        
+
         token.mint(user1, mintAmount);
-        
+
         assertEq(token.balanceOf(user1), mintAmount);
         assertEq(token.totalSupply(), initialSupply + mintAmount);
     }
 
     function testMintOnlyOwner() public {
-        uint256 mintAmount = 5000 * 10**18;
-        
+        uint256 mintAmount = 5000 * 10 ** 18;
+
         vm.prank(user1);
         vm.expectRevert();
         token.mint(user2, mintAmount);
     }
 
     function testBurn() public {
-        uint256 burnAmount = 1000 * 10**18;
+        uint256 burnAmount = 1000 * 10 ** 18;
         uint256 initialBalance = token.balanceOf(owner);
         uint256 initialSupply = token.totalSupply();
-        
+
         token.burn(burnAmount);
-        
+
         assertEq(token.balanceOf(owner), initialBalance - burnAmount);
         assertEq(token.totalSupply(), initialSupply - burnAmount);
     }
@@ -69,6 +69,6 @@ contract SimpleTokenTest is Test {
     function testBurnInsufficientBalance() public {
         vm.prank(user1);
         vm.expectRevert();
-        token.burn(1000 * 10**18);
+        token.burn(1000 * 10 ** 18);
     }
 }
